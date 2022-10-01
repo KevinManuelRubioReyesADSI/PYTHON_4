ad2e8a7dfc25025731b3948d1f69eff4715f45b9
KevinManuelRubioReyesADSI <kevinsolo909@gmail.com>
2022-09-30 20:03:58 -0500

    punto_4

--- main.js
+++ main.js
@@ -1,17 +1,12 @@
-let estudiantes = Number(prompt("Digite los estudiantes a hacer la encuesta"))
-let notaFinal = 0;
-let notaMenor = 10;
-let notaMayor = 0;
-for (let i = 1; estudiantes >= i; i++) {
-    let calificacion = Number(prompt("Ingrese la calificacion que le da a la cafeteria"));
-    notaFinal = notaFinal + calificacion;
-    if (calificacion > notaMayor) {
-        notaMayor = calificacion;
-    } else if (calificacion < notaMenor) {
-        notaMenor = calificacion;
-    } else {
-        notaMenor = calificacion;
-    }
-    let promedio = notaFinal / estudiantes;
-    alert("La calificacion mayor es de " + notaMayor + " la calificacion menor es " + notaMenor + " el promedio total es de " + promedio);
+let mes=20;
+let suma=0;
+let pago = 10;
+
+for (let i=1, j = 21; i<j ; i+=1) {
+    pago=pago*2;
+
+    alert("En el mes " + mes + "El pago fue de " +pago );
+
+    let sumaPago=pago+mes;
+    alert("El total de pago por los 20 meses fue de " +sumaPago);
 }
