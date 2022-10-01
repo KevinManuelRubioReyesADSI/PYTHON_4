a6379b8b6febd8f86403e6c6004ee51326e25c8f
KevinManuelRubioReyesADSI <kevinsolo909@gmail.com>
2022-09-30 19:53:31 -0500

    punto_3

--- main.js
+++ main.js
@@ -1,11 +1,17 @@
-let num=3;
-
-for(let i=0;i<=24;i++){
-    if(i%2==0){
-        num+=4
-        alert(num)
-    }else{
-        num-=2
-        alert(num)
+let estudiantes = Number(prompt("Digite los estudiantes a hacer la encuesta"))
+let notaFinal = 0;
+let notaMenor = 10;
+let notaMayor = 0;
+for (let i = 1; estudiantes >= i; i++) {
+    let calificacion = Number(prompt("Ingrese la calificacion que le da a la cafeteria"));
+    notaFinal = notaFinal + calificacion;
+    if (calificacion > notaMayor) {
+        notaMayor = calificacion;
+    } else if (calificacion < notaMenor) {
+        notaMenor = calificacion;
+    } else {
+        notaMenor = calificacion;
     }
+    let promedio = notaFinal / estudiantes;
+    alert("La calificacion mayor es de " + notaMayor + " la calificacion menor es " + notaMenor + " el promedio total es de " + promedio);
 }
