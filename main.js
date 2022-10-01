1984775529730eb23acd9c53c2716b2955c64033
KevinManuelRubioReyesADSI <kevinsolo909@gmail.com>
2022-09-30 20:35:51 -0500

    punto_9

--- main.js
+++ main.js
@@ -1,12 +1,15 @@
-for(let i=1;i<cantNum;i++){
-    num1=Number(prompt("INGRESE EL PRIMER NUMERO  "))
-    num2=Number(prompt("INGRESE EL SEGUNDO NUMERO  "))
-    let suma=num1+num2
-    let res=num1-num2
-    let mul=num1*num2
-    let div=num1/num2
-    alert("EL RESULTADO DE LA SUMA ES "+""+suma)
-    alert("EL RESULTADO DE LA RESTA ES "+res)
-    alert("EL RESULTADO DE LA MULTIPLICACION ES "+mul)
-    alert("EL RESULTADO DE LA DIVISION ES "+div+"\n")
-}
+const iva=0.16
+const descuento=0.15
+const precio = 100000;
+        let total=0;
+        let cantidad = Number(prompt("Ingrese la cantidad de productos"))
+        for (let i=1; i<=cantidad;i++){
+            total+=precio;
+        }
+        let totalPagar= total+(total*iva);
+        if (totalPagar>500000){
+            let totalDesc = totalPagar-(totalPagar*descuento);
+            alert("El total a pagar es de " +totalDesc);
+        } else {
+            alert("El total a pagar es de "+totalPagar );
+        }
