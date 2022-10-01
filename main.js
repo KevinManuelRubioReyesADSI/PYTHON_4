bbddb81bdba85497535288c4e94828313482e0f6
KevinManuelRubioReyesADSI <kevinsolo909@gmail.com>
2022-09-30 20:24:03 -0500

    punto_6

--- main.js
+++ main.js
@@ -1,17 +1,14 @@
-let ventas = Number(prompt("Ingrese el numero de ventas : "));
-let ventaMenor = 0;
-let ventaMediana = 0;
-let ventaMayor = 0;
-for (let i; i <= ventas; i++) {
-    let precio = Number(prompt("Digite el precio de la venta "));
-    if (precio < 500) {
-        ventaMenor++
-    } else if (precio > 500 && precio < 1000) {
-        ventaMediana++
-    } else {
-        ventaMayor++
-    }
-    alert("Las ventas menores a 500 fueron " + ventaMenor);
-    alert("Las ventas menores a 1000 pero mayores a 500 fueron " + ventaMediana);
-    alert("Las ventas mayores a 1000 fueron " + ventaMayor);
-}
+let trabajadores = Number(prompt("Ingrese el numero de trabajadores"));
+        for (let i=1; i<=trabajadores;i++){
+            let tiempo = Number(prompt("Ingrese el tiempo que lleva en la empresa"));
+            if (tiempo>0 && tiempo<=5){
+                alert("iene un aumento de 100 pesos");
+            } else if (tiempo >5 && tiempo<=10 ){
+                alert("Tiene un aumento de  250 pesos");
+            } else if (tiempo >10 && tiempo<=20 ){
+                alert("Tiene un aumento de 400 pesos");
+            } else {
+                alert("Tiene un aumento de 550 pesos");
+            }
+        }
+alert("Fin del programa")
